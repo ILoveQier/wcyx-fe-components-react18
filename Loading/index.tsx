1da20e481baaa82dbeae2934525a1e908bd653b2
gongsiben <gongsiben@gmail.com>
2023-08-31 12:33:26 +0800

    ggg

--- Loading/index.tsx
+++ Loading/index.tsx
@@ -4,16 +4,16 @@ import "./index.scss";
 export default function FullLoading({
   bgc = "rgba(255,255,255,0.6)",
   children,
-  id='fullLoading',
   cb = () => {},
+  fulstyle={}
 }: any) {
   return (
     <div
       className="fe-full-loading"
-      style={{ background: bgc }}
-      id={id}
+      style={{ background: bgc,...fulstyle }}
+      id="fullLoading"
       onClick={(e: any) => {
-        if (e.target.id == id) {
+        if (e.target.id == "fullLoading") {
           cb(1);
         }
       }}
